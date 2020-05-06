48a2fff926295e22bd307e0a6e920f8fdd9c1ecb
nicholaspung <nicholaspungdev@gmail.com>
2020-05-05 22:42:03 -0700

    Added opening file with editing abilities

--- renderer.js
+++ renderer.js
@@ -1,5 +1,9 @@
 const { ipcRenderer } = require("electron");
 const marked = require("marked");
+const fs = require("fs");
+
+let renderSideMeta = { content: "", path: "" };
+let textAreaMeta = { content: "" };
 
 const renderSide = document.getElementById("render-side");
 const textArea = document.getElementById("edit-area");
@@ -9,15 +13,40 @@ const openFileButton = document.getElementById("open-file");
 const newFileButton = document.getElementById("new-file");
 const saveFileButton = document.getElementById("save-file");
 
-openFileButton.addEventListener("click", (event) => {
+openFileButton.addEventListener("click", () => {
   ipcRenderer.send("open-file-dialog");
 });
 
 ipcRenderer.on("selected-file", (event, path, content) => {
+  // render
   const div = document.createElement("div");
+  div.id = "render-text";
   div.style.overflowY = "scroll";
   div.style.height = "87%";
   div.style.padding = "0.5rem";
   div.innerHTML = marked(content);
   renderSide.appendChild(div);
+  renderSideMeta.content = content;
+  renderSideMeta.path = path;
+
+  // edit
+  textArea.value = content;
+  textAreaMeta.content = content;
+});
+
+textArea.addEventListener("input", (event) => {
+  const renderText = document.getElementById("render-text");
+  if (renderText) {
+    renderText.innerHTML = marked(event.target.value);
+    renderSideMeta.content = event.target.value;
+  }
+});
+
+saveFileButton.addEventListener("click", (event) => {
+  if (renderSideMeta.content === textAreaMeta.content) {
+    console.log("hello");
+    return;
+  }
+  console.log("different");
+  // fs.writeFileSync(textArea.path, renderSideMeta.path);
 });
