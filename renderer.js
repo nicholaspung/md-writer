c27bab7f7b992895cbe4c879dfa780f7378679e5
nicholaspung <nicholaspungdev@gmail.com>
2020-05-07 08:34:13 -0700

    Cleaned up code a bit, want to make it more functional

--- renderer.js
+++ renderer.js
@@ -3,41 +3,85 @@ const marked = require("marked");
 const fs = require("fs");
 const path = require("path");
 
+// state
 let openedFiles = [];
-
-const recentlyOpenFiles = document.getElementById("recently-opened");
+let renderSideMeta = { content: "", path: "" };
+let textAreaMeta = { content: "" };
 const recentlyOpenFilesTxt = path.resolve("./recentlyOpenFiles.txt");
+
+// dom elements
+const mdDisplay = document.getElementById("render-side");
+const editDisplay = document.getElementById("edit-area");
+const openFileButton = document.getElementById("open-file");
+const newFileButton = document.getElementById("new-file");
+const saveFileButton = document.getElementById("save-file");
+const recentDisplay = document.getElementById("recently-opened");
+
+// main
+displayRecentlyOpenFilesTxt();
+
+// event listeners
+openFileButton.addEventListener("click", () => {
+  ipcRenderer.send("open-file-dialog");
+});
+
+editDisplay.addEventListener("input", (event) => {
+  const renderText = document.getElementById("render-text");
+  if (renderText) {
+    renderText.innerHTML = marked(event.target.value);
+    renderSideMeta.content = event.target.value;
+  }
+});
+
+saveFileButton.addEventListener("click", save);
+
+// ipc
+ipcRenderer.on("selected-file", (event, path, content) => {
+  // render
+  if (document.getElementById("render-text")) {
+    const renderText = document.getElementById("render-text");
+    renderText.innerHTML = marked(content);
+  } else {
+    const div = document.createElement("div");
+    div.id = "render-text";
+    div.innerHTML = marked(content);
+    mdDisplay.appendChild(div);
+  }
+  renderSideMeta.content = content;
+  renderSideMeta.path = path[0];
+  // edit
+  editDisplay.value = content;
+  textAreaMeta.content = content;
+  // fs
+  if (!openedFiles.find((p) => p === path[0])) {
+    saveToRecentlyOpenFilesTxt(path[0]);
+    displayRecentlyOpenFilesTxt();
+  }
+});
+
+// helpers
+function save() {
+  if (renderSideMeta.content === textAreaMeta.content) {
+    return;
+  }
+  fs.writeFileSync(renderSideMeta.path, textAreaMeta.content);
+}
+
 function displayRecentlyOpenFilesTxt() {
   if (fs.existsSync(recentlyOpenFilesTxt)) {
     const openFiles = fs.readFileSync(recentlyOpenFilesTxt, "utf8");
     if (openFiles.length > 0) {
-      if (recentlyOpenFiles.childNodes.length > 0) {
-        recentlyOpenFiles.textContent = "";
+      // removes old recentDisplay content
+      if (recentDisplay.childNodes.length > 0) {
+        recentDisplay.textContent = "";
       }
+
       openedFiles = openFiles.split("///n").filter((t) => Boolean(t));
-      console.log(openedFiles);
-      openedFiles.forEach((file) => {
-        const li = document.createElement("li");
-        li.style.padding = "1rem";
-        li.innerHTML = file;
-        recentlyOpenFiles.appendChild(li);
-      });
+      openedFiles.forEach(createRecentFilesNodes);
     }
   } else {
     fs.writeFileSync(recentlyOpenFilesTxt, "");
   }
-}
-displayRecentlyOpenFilesTxt();
-
-function correctOrderWithAddedPath(path) {
-  const pathIndex = openedFiles.findIndex((p) => p === path);
-  if (pathIndex !== -1) {
-    openedFiles.splice(pathIndex, 1);
-  }
-  if (openedFiles.length === 5) {
-    openedFiles.splice(0, 1);
-  }
-  return [...openedFiles, path];
 }
 
 function saveToRecentlyOpenFilesTxt(path) {
@@ -50,57 +94,21 @@ function saveToRecentlyOpenFilesTxt(path) {
   fs.writeFileSync(recentlyOpenFilesTxt, txt);
 }
 
-let renderSideMeta = { content: "", path: "" };
-let textAreaMeta = { content: "" };
-
-const renderSide = document.getElementById("render-side");
-const textArea = document.getElementById("edit-area");
-
-const openFileButton = document.getElementById("open-file");
-const newFileButton = document.getElementById("new-file");
-const saveFileButton = document.getElementById("save-file");
-
-openFileButton.addEventListener("click", () => {
-  ipcRenderer.send("open-file-dialog");
-});
-
-ipcRenderer.on("selected-file", (event, path, content) => {
-  // render
-  if (document.getElementById("render-text")) {
-    const renderText = document.getElementById("render-text");
-    renderText.innerHTML = marked(content);
-  } else {
-    const div = document.createElement("div");
-    div.id = "render-text";
-    div.style.overflowY = "scroll";
-    div.style.height = "87%";
-    div.style.padding = "0.5rem";
-    div.innerHTML = marked(content);
-    renderSide.appendChild(div);
+function correctOrderWithAddedPath(path) {
+  const pathIndex = openedFiles.findIndex((p) => p === path);
+  if (pathIndex !== -1) {
+    openedFiles.splice(pathIndex, 1);
   }
-  renderSideMeta.content = content;
-  renderSideMeta.path = path[0];
-  // edit
-  textArea.value = content;
-  textAreaMeta.content = content;
-  // fs
-  if (!openedFiles.find((p) => p === path[0])) {
-    saveToRecentlyOpenFilesTxt(path[0]);
-    displayRecentlyOpenFilesTxt();
+  if (openedFiles.length === 5) {
+    openedFiles.splice(0, 1);
   }
-});
+  return [...openedFiles, path];
+}
 
-textArea.addEventListener("input", (event) => {
-  const renderText = document.getElementById("render-text");
-  if (renderText) {
-    renderText.innerHTML = marked(event.target.value);
-    renderSideMeta.content = event.target.value;
-  }
-});
-
-saveFileButton.addEventListener("click", (event) => {
-  if (renderSideMeta.content === textAreaMeta.content) {
-    return;
-  }
-  fs.writeFileSync(renderSideMeta.path, textAreaMeta.content);
-});
+function createRecentFilesNodes(file) {
+  // for windows
+  const display = file.slice(file.lastIndexOf("\\") + 1, file.length);
+  const li = document.createElement("li");
+  li.innerHTML = display;
+  recentDisplay.appendChild(li);
+}
